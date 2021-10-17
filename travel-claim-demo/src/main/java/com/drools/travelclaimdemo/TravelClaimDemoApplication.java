1601c56aa6006ff6bdeb4a31c19208ac8c6f7a5a
Sriram Venkat <v.sriram.1987@gmail.com>
2021-10-17 16:12:53 +0530

    drools demo

---
+++ travel-claim-demo/src/main/java/com/drools/travelclaimdemo/TravelClaimDemoApplication.java
@@ -0,0 +1,13 @@
+package com.drools.travelclaimdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelClaimDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelClaimDemoApplication.class, args);
+	}
+
+}
---
+++ travel-claim-demo/src/test/java/com/drools/travelclaimdemo/TravelClaimDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drools.travelclaimdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelClaimDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
